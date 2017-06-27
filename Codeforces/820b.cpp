47ebc1e6f4d17e2e807d499a31869192fa7ab629
dylan007 <dylandey1996@gmail.com>
2017-06-28 00:15:45 +0530

    CF 820 div2

---
+++ Codeforces/820b.cpp
@@ -0,0 +1,71 @@
+//Shounak Dey
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define PB push_back
+#define MK make_pair 
+#define SZ(a) (int)(sizeof(a))
+#define F first
+#define S second
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define REP(i,n) for(int i=0;i<n;i++)
+#define read(n) scanf("%d",&n)
+#define readl(n) scanf("%lld",&n)
+#define readul(n) scanf("%llu",&n);
+
+
+#define SORTV(a) sort(a.begin(),a.end())
+#define TEST int Test;cin >> Test;while(Test--)
+#define gcd(a,b) __gcd(a,b)
+#define ffs(a) __builtin_ffs(a) // find first set
+#define clz(a) __builtin_clz(a) // count leading zeroes
+#define ctz(a) __builtin_ctz(a) // count trailing zeroes
+#define popc(a) __ builtin_popcount(a) // count set bits
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+
+#define error(args...) { vector<string> _v = split(#args, ','); err(_v.begin(), args); }
+
+vector<string> split(const string& s, char c) {
+	vector<string> v;
+	stringstream ss(s);
+	string x;
+	while (getline(ss, x, c))
+		v.emplace_back(x);
+	return move(v);
+}
+
+void err(vector<string>::iterator it) {}
+template<typename T, typename... Args>
+void err(vector<string>::iterator it, T a, Args... args) {
+	cerr << it -> substr((*it)[0] == ' ', it -> length()) << " = " << a << '\n';
+	err(++it, args...);
+}
+
+int main()
+{
+	double n,a;
+	cin >> n >> a;
+	if(n==3)
+		cout << "1 2 3" << endl;
+	else
+	{
+		string out = "1 2 ";
+		double p,ang = (180.00)/n;
+		int ans;
+		p = round(a/ang);
+		if(p==0)
+			p++;
+		if(p>(n-2))
+			p=n-2;
+		p = n+1-p;
+		cout << out;
+		cout << p << endl;
+	}
+	return 0;
+}
+
